5ed5891f0bcaa63b180fd4c9266494e6efb3120b
Wilton Rodrigues <wiltonsr94@gmail.com>
2017-08-16 21:39:58 -0300

    Adicionando comentários ao código

--- 4_Codigos_MSP-430-G2-553/blink.c
+++ 4_Codigos_MSP-430-G2-553/blink.c
@@ -3,10 +3,19 @@
 
 void main(void)
 {
+  /* Stop watchdog timer */
   WDTCTL = WDTPW + WDTHOLD;
+
+  /* Set LEDS (BIT0 and BIT6) as output
+   * and others bits as input */
   P1DIR = LEDS;
+
+  /* P1.3 is connected to the button. In order to make the button work,
+   * we have to add pull-up resistor to make it normally high. */
   P1REN = BIT3;
   P1OUT = BIT3;
+
+  /* Loop */
   while(1)
   {
     if((P1IN & BIT3)!=BIT3)
