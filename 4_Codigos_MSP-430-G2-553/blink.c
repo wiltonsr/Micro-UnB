22d8ee6cfa70a8741b9a8cf769145ed50544ea4b
Wilton Rodrigues <wiltonsr94@gmail.com>
2017-09-16 16:34:38 -0300

    Changing hello world example

--- 4_Codigos_MSP-430-G2-553/blink.c
+++ 4_Codigos_MSP-430-G2-553/blink.c
@@ -1,27 +1,14 @@
 #include <msp430g2553.h>
-#define LEDS (BIT0 + BIT6)
+#define LED1 BIT0
+#define LED2 BIT6
+#define LEDS (LED1|LED2)
+#define BTN  BIT3
 
-void main(void)
+int main(void)
 {
-  /* Stop watchdog timer */
-  WDTCTL = WDTPW + WDTHOLD;
-
-  /* Set LEDS (BIT0 and BIT6) as output
-   * and others bits as input */
-  P1DIR = LEDS;
-
-  /* P1.3 is connected to the button. In order to make the button work,
-   * we have to add pull-up resistor to make it normally high. */
-  P1REN = BIT3;
-  P1OUT = BIT3;
-
-  /* Loop */
-  while(1)
-  {
-    if((P1IN & BIT3)!=BIT3)
-    {
-      __delay_cycles(220000);
-      P1OUT ^= LEDS;
-    }
-  }
+	WDTCTL = WDTPW | WDTHOLD;
+	P1OUT |= LEDS;
+	P1DIR |= LEDS;
+	while(1);
+	return 0;
 }
