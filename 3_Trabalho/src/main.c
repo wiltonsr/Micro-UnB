d2569c3aecef149d18de8ad02572605f502d1625
Wilton Rodrigues <wiltonsr94@gmail.com>
2017-11-11 17:36:13 -0200

    Finalizada interrupções de direção

--- 3_Trabalho/src/main.c
+++ 3_Trabalho/src/main.c
@@ -3,34 +3,48 @@
 #include "drawing.h"
 #include <msp430g2553.h>
 #include <legacymsp430.h> // Para rodar interrupcoes
-#define BTN BIT6
+#define STOP_BTN BIT6
+#define RIGHT_BTN BIT0
+#define LEFT_BTN BIT6
 
 int main(){
-  WDTCTL = WDTPW + WDTHOLD;   // Disable WDT
+  WDTCTL = WDTPW + WDTHOLD;   // Desabilita WDT
   DCOCTL = CALDCO_1MHZ;     // 1 Mhz DCO
   BCSCTL1 = CALBC1_1MHZ;
 
   initialise();
-  P1DIR &= ~BTN;
-  P1REN &= ~BTN;
-  P1OUT |= BTN;
-  P1IES |= BTN;
-  P1IE |= BTN;
-  _BIS_SR(GIE);
+
+  P1DIR &= ~(RIGHT_BTN + LEFT_BTN); //Seta como entrada 0 = entrada
+  P1OUT &= ~(RIGHT_BTN + LEFT_BTN); //Desliga ambos os leds
+  P1IE |= (RIGHT_BTN + LEFT_BTN);
+  P1IFG &= ~(RIGHT_BTN + LEFT_BTN);
+  P1REN = (RIGHT_BTN + LEFT_BTN);
+  P1IES |= (RIGHT_BTN + LEFT_BTN);
+
+
+  __enable_interrupt(); // enable all interrupts
 
   clear_screen();
 
   while(1){
     ahead_arrow();
-    P1OUT |= 0xFF;
   }
 
   return 0;
 }
 
-interrupt(PORT1_VECTOR) Interrupcao_P1(void){
-  while((P1IN&BTN)==0){
-    stop();
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void){
+  if(P1IFG & RIGHT_BTN){
+    while(!(P1IN & RIGHT_BTN)==0){
+      right_arrow();
+    }
+    P1IFG &= ~RIGHT_BTN;
   }
-  P1IFG &= ~BTN;
+  if(P1IFG & LEFT_BTN){
+    while(!(P1IN & LEFT_BTN)==0){
+      left_arrow();
+    }
+    P1IFG &= ~LEFT_BTN;
+  }
 }
