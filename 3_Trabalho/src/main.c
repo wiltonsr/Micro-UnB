0ff968dc3b4df0b63bb8dc58b59b1efbb5fa6563
Wilton Rodrigues <wiltonsr94@gmail.com>
2017-11-29 09:01:12 -0200

    Esquemático de ligação dos pinos

--- 3_Trabalho/src/main.c
+++ 3_Trabalho/src/main.c
@@ -4,6 +4,22 @@
 #include <msp430g2553.h>
 #include <legacymsp430.h> // Para rodar interrupcoes
 
+/*
+                MSP430G2553
+            -------------------
+            |VCC           GND|
+RIGHT_BTN-->|P1.0          XIN|
+            |P1.1         XOUT|
+            |P1.2          TST|
+      DIN<--|P1.3          RST|
+       CS<--|P1.4         P1.7|<--STOP_BTN
+      CLK<--|P1.5         P1.6|<--LEFT_BTN
+            |P2.0         P2.5|
+            |P2.1         P2.4|
+            |P2.2         P2.3|
+            -------------------
+*/
+
 int main(){
   WDTCTL = WDTPW + WDTHOLD;   // Desabilita WDT
   DCOCTL = CALDCO_1MHZ;     // 1 Mhz DCO
